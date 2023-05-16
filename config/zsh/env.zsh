8037463a1e792ee3c53b4f65836145d692fdba54
5nik7 <nickflip86@gmail.com>
2023-05-16 01:43:04 -0700

    hist

--- config/zsh/env.zsh
+++ config/zsh/env.zsh
@@ -14,6 +14,7 @@ export BROWSER="chromium"
 export VISUAL="nvim"
 export EDITOR="nvim"
 export GTK_THEME="Kanagawa-Borderless"
+export GDK_SCALE=2
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
