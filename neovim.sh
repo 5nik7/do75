b55e24d167acc7f2059082d093aa3792f33b6b8a
5nik7 <nickflip86@gmail.com>
2023-05-17 18:04:01 -0700

    .

---
+++ neovim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+say 'Building neovim.'
+src=https://github.com/neovim/neovim
+dst=$HOME/source/neovim
+
+if [[ -d ${dst} ]]; then
+  echo 'Update neovim sources.'
+  cd ${dst}
+    git pull
+else
+  echo 'Clone neovim sources.'
+  git clone $src $dst
+fi
+
+echo 'Build neovim.'
+cd ${dst}
+sudo make CMANE_BUILD=RelWithDebInfo install
+cd $HOME
