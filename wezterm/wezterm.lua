a820132493386ee99599ddf358d57d93c02ae138
5nik7 <nickflip86@gmail.com>
2023-06-24 03:46:04 -0700

    term

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,46 +1,45 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 return {
 
-  font = wezterm.font("JetBrainsMono Nerd Font Propo", {weight="Medium", stretch="Normal", style="Normal"}),
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" }),
 
-  font_rules = {
-    {
-      italic = true,
-      font = wezterm.font("JetBrainsMono Nerd Font Propo", {weight="Medium", stretch="Normal", style="Italic"}),
-    },
-    {
-      intensity = "Bold",
-      font = wezterm.font("JetBrainsMono Nerd Font Propo", {weight="Bold", stretch="Normal", style="Normal"}),
-    },
-    {
-      intensity = "Bold",
-      italic = true,
-      font = wezterm.font("JetBrainsMono Nerd Font Propo", {weight="Bold", stretch="Normal", style="Italic"})
-    },
-  },
+	font_rules = {
+		{
+			italic = true,
+			font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium", stretch = "Normal", style = "Italic" }),
+		},
+		{
+			intensity = "Bold",
+			font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+		},
+		{
+			intensity = "Bold",
+			italic = true,
+			font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+		},
+	},
 
-  font_size = 10.0,
+	font_size = 10.0,
 
-  color_scheme = "Catppuccin",
+	color_scheme = "Catppuccin",
 
-  colors = {
-		indexed = {[16] = "#F8BD96", [17] = "#F5E0DC"},
+	colors = {
+		indexed = { [16] = "#F8BD96", [17] = "#F5E0DC" },
 		split = "#161320",
 		visual_bell = "#302D41",
 	},
-  window_padding = {
-    left = 15,
-    right = 15,
-    top = 10,
-    bottom = 0,
-  },
+	window_padding = {
+		left = 20,
+		right = 20,
+		top = 20,
+		bottom = 20,
+	},
 
-  window_background_opacity = 0.98,
-  window_decorations = "RESIZE",
-  enable_tab_bar = false,
-  scrollback_lines = 5000,
-  enable_scroll_bar = false,
-  check_for_updates = false,
+	window_background_opacity = 0.98,
+	window_decorations = "RESIZE",
+	enable_tab_bar = false,
+	scrollback_lines = 5000,
+	enable_scroll_bar = false,
+	check_for_updates = false,
 }
-
