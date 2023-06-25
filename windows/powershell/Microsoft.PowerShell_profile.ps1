8f4e8d98fefef40d29b344247c02d7f67f591125
5nik7 <nickflip86@gmail.com>
2023-06-24 17:08:58 -0700

    catps

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,58 @@ if ($host.Name -eq 'ConsoleHost') {
     Import-Module PSReadLine
 }
 
+Import-Module Catppuccin
+
+$Flavor = $Catppuccin['Mocha']
+
+$Colors = @{
+    # Largely based on the Code Editor style guide
+    # Emphasis, ListPrediction and ListPredictionSelected are inspired by the Catppuccin fzf theme
+
+    # Powershell colours
+    ContinuationPrompt     = $Flavor.Teal.Foreground()
+    Emphasis               = $Flavor.Red.Foreground()
+    Selection              = $Flavor.Surface0.Background()
+
+    # PSReadLine prediction colours
+    InlinePrediction       = $Flavor.Overlay0.Foreground()
+    ListPrediction         = $Flavor.Mauve.Foreground()
+    ListPredictionSelected = $Flavor.Surface0.Background()
+
+    # Syntax highlighting
+    Command                = $Flavor.Blue.Foreground()
+    Comment                = $Flavor.Overlay0.Foreground()
+    Default                = $Flavor.Text.Foreground()
+    Error                  = $Flavor.Red.Foreground()
+    Keyword                = $Flavor.Mauve.Foreground()
+    Member                 = $Flavor.Rosewater.Foreground()
+    Number                 = $Flavor.Peach.Foreground()
+    Operator               = $Flavor.Sky.Foreground()
+    Parameter              = $Flavor.Pink.Foreground()
+    String                 = $Flavor.Green.Foreground()
+    Type                   = $Flavor.Yellow.Foreground()
+    Variable               = $Flavor.Lavender.Foreground()
+}
+
+# Set the colours
+Set-PSReadLineOption -Colors $Colors
+
+$PSStyle.Formatting.Debug = $Flavor.Sky.Foreground()
+$PSStyle.Formatting.Error = $Flavor.Red.Foreground()
+$PSStyle.Formatting.ErrorAccent = $Flavor.Blue.Foreground()
+$PSStyle.Formatting.FormatAccent = $Flavor.Teal.Foreground()
+$PSStyle.Formatting.TableHeader = $Flavor.Rosewater.Foreground()
+$PSStyle.Formatting.Verbose = $Flavor.Yellow.Foreground()
+$PSStyle.Formatting.Warning = $Flavor.Peach.Foreground()
+
+$ENV:FZF_DEFAULT_OPTS = @"
+--color=bg+:$($Flavor.Surface0),bg:$($Flavor.Base),spinner:$($Flavor.Rosewater)
+--color=hl:$($Flavor.Red),fg:$($Flavor.Text),header:$($Flavor.Red)
+--color=info:$($Flavor.Mauve),pointer:$($Flavor.Rosewater),marker:$($Flavor.Rosewater)
+--color=fg+:$($Flavor.Text),prompt:$($Flavor.Mauve),hl+:$($Flavor.Red)
+--color=border:$($Flavor.Surface2)
+"@
+
 Set-PSReadLineKeyHandler -Chord '"', "'" `
     -BriefDescription SmartInsertQuote `
     -LongDescription "Insert paired quotes if not already on a quote" `
@@ -35,9 +87,6 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineOption -HistorySearchCursorMovesToEnd
-Set-PSReadLineOption -Colors @{
-    "InlinePrediction" = [ConsoleColor]::DarkGray;
-}
 
 # PSFzf
 Set-PSFzfOption -PSReadLineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl+r'
